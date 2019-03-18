69b3de1e447e276f21bfb2d2315e1a9ad12a1e1d
makkalot <makkalot@gmail.com>
2019-03-18 12:56:51 +0200

    remove etcd refs from producer

--- jobs/producer.go
+++ jobs/producer.go
@@ -2,7 +2,9 @@ package jobs
 
 import (
 	"context"
-	"go.etcd.io/etcd/clientv3"
+	"encoding/json"
+	"github.com/BeameryHQ/async-stream/kvstore"
+	"strings"
 )
 
 type Producer interface {
@@ -10,7 +12,43 @@ type Producer interface {
 	SubmitWithRetry(ctx context.Context, taskName string, args JobParameters, maxRetry int) (string, error)
 }
 
-type EtcdProducer struct {
-	cli  *clientv3.Client
+type producerProvider struct {
+	cli  kvstore.Store
 	path string
+}
+
+func (p *producerProvider) fullPath(jobId string) string {
+	return strings.Join([]string{p.path, jobId, "data"}, "/")
+}
+
+func (p *producerProvider) Submit(ctx context.Context, taskName string, args JobParameters) (string, error) {
+	jobId, j := NewJob(taskName, args)
+	jobPayload, err := json.Marshal(j)
+	if err != nil {
+		return "", err
+	}
+
+	jobPath := p.fullPath(jobId)
+	err = p.cli.Put(ctx, jobPath, string(jobPayload), kvstore.WithNoLease())
+	if err != nil {
+		return "", err
+	}
+
+	return jobId, nil
+}
+
+func (p *producerProvider) SubmitWithRetry(ctx context.Context, taskName string, args JobParameters, maxRetry int) (string, error) {
+	jobId, j := NewJobWithRetry(taskName, args, maxRetry)
+	jobPayload, err := json.Marshal(j)
+	if err != nil {
+		return "", err
+	}
+
+	jobPath := p.fullPath(jobId)
+	err = p.cli.Put(ctx, jobPath, string(jobPayload), kvstore.WithNoLease())
+	if err != nil {
+		return "", err
+	}
+
+	return jobId, nil
 }
